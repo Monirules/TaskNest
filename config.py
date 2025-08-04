22c8951f3f21ef44971838546233a7d0c86e22af
Monirul Islam <monirul.mahmud@northsouth.edu>
2025-08-04 12:39:19 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,10 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@"
-        f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}?ssl-mode=REQUIRED"
+        f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}?ssl_mode=REQUIRED"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "default-secret")
+
 
 
