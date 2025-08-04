5c419435d6b3a6130f9df069a3c74f3cd251909d
Monirul Islam <monirul.mahmud@northsouth.edu>
2025-08-04 11:56:22 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'this_should_be_secret'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '2532'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://mim9:mahmud123@localhost/todolist_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
