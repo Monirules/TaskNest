44632c093b43713cb11b078d9955a700636c8281
Monirul Islam <monirul.mahmud@northsouth.edu>
2025-08-04 11:52:56 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,6 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'change_this_secret')
-    SQLALCHEMY_DATABASE_URI = (
-        os.environ.get('DATABASE_URL') or
-        'mysql://root@localhost:3306/todolist_db'
-    )
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'this_should_be_secret'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://mim9:mahmud123@localhost/todolist_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
